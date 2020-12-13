1cef84cfe8c55e9e26d10e1dea22d5e82fab2af2
Martin K <martin.k.io.github@gmail.com>
2020-12-13 21:34:16 +0200

    Update the new_item_window and add ItemListBoxRow

--- resources/ui/application_window.rb
+++ resources/ui/application_window.rb
@@ -6,9 +6,12 @@ module Todo
     class << self
       def init
         # Set the template from the resources binary
-        set_template resource: '/com/martink-i-o/gtk-todo-app/ui/application_window.ui'
+        set_template(
+          resource: '/com/martink-i-o/gtk-todo-app/ui/application_window.ui'
+        )
 
-        bind_template_child 'add_new_item_button'
+        bind_template_child('add_new_item_button')
+        bind_template_child('todo_items_list_box')
       end
     end
 
@@ -23,6 +26,27 @@ module Todo
           Todo::Item.new(user_data_path: application.user_data_path)
         )
         new_item_window.present
+      end
+
+      load_todo_items
+    end
+
+    def load_todo_items
+      # Clear all children initially in order to show up-to-date notes
+      todo_items_list_box.children.each do |child|
+        todo_items_list_box.remove child
+      end
+
+      json_files = Dir[File.join(File.expand_path(application.user_data_path), '*.json')]
+
+      # Create new Todo::Item for each existing file
+      items = json_files.map do |filename|
+        Todo::Item.new(filename: filename)
+      end
+
+      # Add all items to the Todo::ItemListBox via Todo::ItemListBoxRow
+      items.each do |item|
+        todo_items_list_box.add(Todo::ItemListBoxRow.new(item))
       end
     end
   end
