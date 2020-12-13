5e3d040611f5fb164e63dde2c0a35e120af29b5e
Martin K <martin.k.io.github@gmail.com>
2020-12-13 16:37:14 +0200

    Bind the widgets

--- app/ui/todo/new_item_window.rb
+++ app/ui/todo/new_item_window.rb
@@ -7,6 +7,14 @@ module Todo
       def init
         # Set the template from the resources binary
         set_template(resource: '/com/martink-i-o/gtk-todo-app/ui/new_item_window.ui')
+
+        # Bind the window's widgets
+        bind_template_child 'id_value_label'
+        bind_template_child 'title_text_entry'
+        bind_template_child 'notes_text_view'
+        bind_template_child 'priority_combo_box'
+        bind_template_child 'cancel_button'
+        bind_template_child 'save_button'
       end
     end
 
